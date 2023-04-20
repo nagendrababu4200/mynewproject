bedd5155c7362c3523f46ce11f9f4400451ee354
Nag4200 <97871042+Nag4200@users.noreply.github.com>
2023-04-20 10:35:32 +0530

    change order

--- nagendrababu.py
+++ nagendrababu.py
@@ -2,5 +2,5 @@ from awsglue.transforms import *
 
 #Orders Tranformation
 def tansform(df):
-  df = SelectFields.apply(df, paths=['ord_no','purch_amt']) 
+  df = SelectFields.apply(df, paths=['ord','purch_amt']) 
   return df
