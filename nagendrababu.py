551dc90a664464696ea0bab906524fe8f2217c16
Nag4200 <97871042+Nag4200@users.noreply.github.com>
2023-04-20 10:40:15 +0530

    vgff

--- nagendrababu.py
+++ nagendrababu.py
@@ -2,5 +2,5 @@ from awsglue.transforms import *
 
 #Orders Tranformation
 def tansform(df):
-  df = SelectFields.apply(df, paths=['ord','purch_amt']) 
+  df = SelectFields.apply(df, paths=['ordsss','purch_amt']) 
   return df
